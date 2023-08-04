6b8a6d53754fd780474b4c12096ca3fc85a3fc68
Samson Tush <samo4421@icloud.com>
2023-08-04 21:18:20 +0300

    Add database and controllers

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# db/seeds.rb
+
+greetings_list = [
+  "Hello!",
+  "Hi there!",
+  "Greetings!",
+  "Welcome!",
+  "Salutations!",
+  "Hey!",
+  "Good day!",
+  "Howdy!"
+]
+
+greetings_list.each do |greeting|
+  Greeting.create(message: greeting)
+end
